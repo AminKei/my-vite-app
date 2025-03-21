a77dac380249c27e9f9abff2c55e5a6c76a9cd24
skeimasi <skeimasi.devel@gmail.com>
2025-03-21 15:16:21 +0330

    added css module

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "tailwindcss";
 import { Provider } from "react-redux";
 import store from "./store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
